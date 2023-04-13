3e45650de51fd4b77aa28d08a3d5b16b3ce48854
Tonko <tbosse20@student.aau.dk>
2023-04-13 13:21:49 +0200

    AudioManager

--- BCI Training/Assets/Scripts/Enemy.cs
+++ BCI Training/Assets/Scripts/Enemy.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Unit
-{
+public class Enemy : Unit {
 
     public new Action action = Action.Patroling;
 
