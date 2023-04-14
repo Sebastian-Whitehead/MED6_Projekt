acd84791e889b0bfee3a241c163e8007d711bbb6
Tonko <tbosse20@student.aau.dk>
2023-04-14 22:02:21 +0200

    Update Unit.cs

--- BCI Training/Assets/Scripts/Unit.cs
+++ BCI Training/Assets/Scripts/Unit.cs
@@ -6,7 +6,7 @@ public abstract class Unit : PlayerMove {
 
     protected TurnManager turnManager;
 
-    public enum Action {
+    public enum Action { // Possible actions
         Idle,
         Patroling,
         Attacking,
@@ -15,45 +15,49 @@ public abstract class Unit : PlayerMove {
         Investegating
     };
     
-    public Action action;
-    protected bool hasAttacked = false;
+    public Action action; // Current action
+    protected bool hasAttacked = false; // Has attacked this turn
 
-    [Header("Character")]
+    [Header("Character")] // 
     public float maxHealth = 10f;
-    public float attackRange = 1f;
-    public float damage = 2f;
+    public float attackRange = 1f; // Range of attack
+    public float damage = 2f; // Damage target on attack
     public float health;
-    public bool alive = true;
-    public bool offensive = false;
+    public bool alive = true; // Unit is alive
+    public bool offensive = false; // Spot and attack targets
 
-    [Header("View")]
-    [Range(2, 15)] public int inc = 5;
-    [Range(1, 180)] public int FOV = 25;
-    [Range(0f, 100f)] public float distance = 10f;
-    public string targetTag;
-    protected Vector3 targetLocation;
-    protected Unit target;
+    [Header("View")] // Viewing propeties
+    [Range(2, 15)] public int inc = 5; // Increment and rate of raycast
+    [Range(1, 180)] public int FOV = 25; // Field of view [1-180]
+    [Range(0f, 100f)] public float distance = 10f; // Raycast/view distance
+    public string targetTag; // Tag unit will target and attack
+    protected Vector3 targetLocation; // Target location to go to (check usage)
+    protected Unit target; // Target to attack
 
-    [Header("Debug")]
-    protected Color viewColor = Color.green;
-    protected Color moveColor;
-    protected bool execute;
+    [Header("Debug")] // Gizmo propeties
+    protected Color viewColor = Color.green; // Visualize eyes raycast
+    protected Color moveColor; // Visualize moving 
+    protected bool execute; // Execution of current action
 
-    [Header("Manager")]
-    public bool active = false;
+    [Header("Manager")] // Managing variables
+    public bool active = false; // Actions are possible
+
+    // ---------------------------------------------------------------------
+
+    // Abstrac methods
     public abstract void TakeDamage(Vector3 hitPosition, float damageTaken);
-    protected abstract void UnitGone();
-    protected abstract void ChildAwake();
-    protected abstract void ChildUpdate();
-    public abstract void DecisionTree();
-    protected abstract bool AttackCheck();
+    protected abstract void UnitGone(); // Target is gone after marked
+    protected abstract void ChildAwake(); // Sub-class method of 'Awake'
+    protected abstract void ChildUpdate(); // Sub-class method of 'Update'
+    public abstract void DecisionTree(); // Enemy decision tree to pick action
+    protected abstract bool AttackCheck(); // Check possible attack
 
     void Awake() {
         turnManager = GameObject.Find("GameManager").GetComponent<TurnManager>();
         //target = GameObject.FindGameObjectWithTag(targetTag).GetComponent<Unit>();
-        target = null;
+        target = null; // Null set target
         health = maxHealth;
-        ChildAwake();
+        ChildAwake(); // Sub-class method call
     }
 
     public void Update() {
@@ -62,43 +66,44 @@ public abstract class Unit : PlayerMove {
         ChildUpdate(); // Subclasses Update method
         if (!execute && tag == "Player") return; // Execute on confirm
         AttackTarget(); // Attack target, if set
-        DecisionTree();
+        DecisionTree(); // Decision tree to pick next action
         if (!isMoving) return; // Break when not moving
         Move(); // Move to target tile
         if (path.Count <= 0) Deactivate(); // Deactivate when at location
     }
 
+    // Positive and alive method
     protected void Alive() {
-        health = Mathf.Max(health, 0);
-        if (health > 0) return;
-        alive = false;
-        Destroy(gameObject);
+        if (health > 0) return; // Break at positive health
+        health = Mathf.Max(health, 0); // Zero minimalize health value
+        alive = false; // Disable living
+        Destroy(gameObject); // Destroy game object (Temporary until animation)
     }
 
+    // Eyes to spot game objects matching with 'target tag'
     private void Eyes() {
-        if (!offensive) return;
+        if (!offensive) return; // Break at not offensive
         
-        inc = Mathf.Max(2, inc);
+        inc = Mathf.Max(2, inc); // Minimalize increments
         RaycastHit hit;
 
-        Vector3 dir = targetLocation - transform.position;
-        Debug.DrawRay(transform.position, dir, viewColor);
-
         for (int angle = -FOV; angle <= FOV; angle += inc) {
-            Vector3 targetPos = new Vector3(0, 0, 0);
+            Vector3 targetPos = new Vector3(0, 0, 0); // Intilize a zero-vector
+            // Get angle from for-loop and object forward direction
             targetPos += Quaternion.AngleAxis(angle, Vector3.up) * transform.forward * distance;
-            Debug.DrawRay(transform.position, targetPos, viewColor);
+            Debug.DrawRay(transform.position, targetPos, viewColor); // Visualize raycast
             if (!Physics.Raycast(transform.position, targetPos, out hit, distance)) continue;
-            if (hit.transform.tag != targetTag) continue;
-            SetTarget(hit.transform);
-            return;
+            if (hit.transform.tag != targetTag) continue; // Only matching 'target tag'
+            SetTarget(hit.transform); // Set target to hit game object
+            return; // Break loop
         }
-        UnitGone();
+        UnitGone(); // Call when unit disapereard
     }
 
+    // Have unit go idle 
     private void Idle() {
         action = Action.Idle;
-        targetLocation = transform.position;
+        targetLocation = transform.position; // Target location to current location
     }
 
     // Set target to chase/attack
@@ -107,7 +112,7 @@ public abstract class Unit : PlayerMove {
         targetLocation = tmpTarget.position; // Get target location
         action = Action.Attacking;
         Debug.Log(transform.name + " set target: " + target.name);
-        if (tag == "Enemy") Deactivate();
+        if (tag == "Enemy") Deactivate(); // Deactivate if unit is 'Enemy'
     }
 
     // Attack target, if set and close enough
@@ -126,36 +131,42 @@ public abstract class Unit : PlayerMove {
         Deactivate(); // Deactivate unit
     }
 
+    // Check if distance to given position is matching attack range 
     private bool OutOfRange(Vector3 targetPos) {
-        Vector3 playerPos = gameObject.transform.position;
+        Vector3 playerPos = gameObject.transform.position; 
+         // Calc. distance from unit to target
         float distance = Vector3.Distance(playerPos, targetPos);
-        if (distance > attackRange) {
+        if (distance > attackRange) { // Break at distance more than range
             Debug.Log("Out of range");
-            BFS();
-            MoveTo(GetTileAtPosition(targetPos)); 
-            return true;
+            // Walk to target instead
+            BFS(); // Breath search tiles
+            MoveTo(GetTileAtPosition(targetPos)); // Move towards target
+            return true; // Abort method
         }
-        return false; 
+        return false; // Confirm method
     }
 
     // ---------------------------------------------------------------------
 
+    // Activate unit
     public void Activate() {
-        active = true;
-        hasAttacked = false;
-        steps = moveRange;
-        turnManager.Wait();
+        active = true; // Reset active
+        hasAttacked = false; // Reset attacked
+        steps = moveRange; // Reset steps
+        turnManager.Wait(); // TurnManager wait for unit to finish execution
     }
 
+    // Deactivate unit 
     public void Deactivate() {
         if ((tag == "Enemy" && (steps <= 0 || !isMoving)) || 
         (tag == "Player" && steps <= 0) || hasAttacked) {
-            active = false;
-            isMoving = false;
-            steps = 0;
+            active = false; // Disable activity
+            isMoving = false; // Disable moving
+            steps = 0; // Remove all steps
         }
     }
 
+    // Return activity
     public bool Active() {
         return active;
     }
